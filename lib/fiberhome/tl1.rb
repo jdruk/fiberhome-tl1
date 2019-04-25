025da95fdf8b28e98d6a049c236241842ff916bf
jdruk <devjosafa@gmail.com>
2019-04-25 13:36:43 -0300

    commit inicial

---
+++ lib/fiberhome/tl1.rb
@@ -0,0 +1,8 @@
+require "fiberhome/tl1/version"
+
+module Fiberhome
+  module Tl1
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/fiberhome/tl1/version.rb
@@ -0,0 +1,5 @@
+module Fiberhome
+  module Tl1
+    VERSION = "0.1.0"
+  end
+end
